bffaf45e6a9fc89cc061b59969e86b08d529b237
Gabriella <gabriellazamudio@yahoo.se>
2020-06-02 13:05:25 +0200

    api style and cleanup

--- script/main.js
+++ script/main.js
@@ -1,29 +1,29 @@
 const app = new Vue({
   el: '#search', 
   data: {
-    findSongs: [], 
-    searchArtist: '', 
-    modal2: false, 
-    songLyrics: []
+    findSong: [], 
+    findArtist: '', 
+    modal: false, 
+    findLyrics: []
   },
   methods: {
     handleSubmit: function(){
-      fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track_artist='+this.searchArtist+'&page_size=10&page=1&s_track_rating=desc&f_has_lyrics=1&apikey=b423b10f9ea2ae8ac7a91deda0c4ffa7')
+      fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_track_artist='+this.findArtist+'&page_size=10&page=1&s_track_rating=desc&f_has_lyrics=1&apikey=b423b10f9ea2ae8ac7a91deda0c4ffa7')
     .then(response => response.json())
-    .then(findSongsResponce => {
-      this.findSongs = findSongsResponce.message.body.track_list; 
+    .then(findSongResponce => {
+      this.findSong = findSongResponce.message.body.track_list; 
     });
 
     }, 
-    modalAction2: function(track) {
-      if(this.modal2 == false){
-        this.modal2 = true; 
-        fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id='+track+'&apikey=b423b10f9ea2ae8ac7a91deda0c4ffa7')
+    modalAction: function(song) {
+      if(this.modal == false){
+        this.modal = true; 
+        fetch('https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id='+song+'&apikey=b423b10f9ea2ae8ac7a91deda0c4ffa7')
         .then(response => response.json())
-        .then(songLyricsResponce => {
-        this.songLyrics = songLyricsResponce.message.body.lyrics}) 
+        .then(findLyricsResponce => {
+        this.findLyrics = findLyricsResponce.message.body.lyrics}) 
       } else {
-        this.modal2 = false
+        this.modal = false
       } 
     }
   }
